346f08eb66110563cdb894f57588f7fd30e99b58
hiroto-yagi <hero10.4eep@gmail.com>
2024-08-06 13:10:58 +0900

    #ex01-firstCommit

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,10 +1,47 @@
 package com.example.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Administrator;
+import com.example.form.InsertAdministratorForm;
+import com.example.service.AdministratorService;
+
+/**
+ * 管理者登録用Conrtroller.
+ */
 @Controller
-@RequestMapping("/administrator")
+@RequestMapping("/")
 public class AdministratorController {
-    
+    @Autowired
+    AdministratorService administratorService;
+
+    @GetMapping("/toInsert")
+    public String toInsert(InsertAdministratorForm form, Model model) {
+        model.addAttribute("name", form.getName());
+        model.addAttribute("mailAddress", form.getMailAddress());
+        model.addAttribute("password", form.getPassword());
+        return "administrator/insert";
+    }
+
+    // /**
+    //  * 管理者情報登録.
+    //  * @param form 管理者情報フォーム
+    //  * @return ログイン画面
+    //  */
+    // @PostMapping("/insert")
+    // public String insert(InsertAdministratorForm form) {
+    //     Administrator administrator = new Administrator();
+    //     // formオブジェクトからadministratorオブジェクトにプロパティ値をコピー
+    //     BeanUtils.copyProperties(form, administrator);
+    //     administratorService.insert(administrator);
+    //     return "redirect:/toInsert";
+    // }
+
+
 }
--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -2,7 +2,37 @@ package com.example.form;
 
 import org.springframework.validation.annotation.Validated;
 
+/**
+ * 管理者情報登録フォーム.
+ */
 @Validated
 public class InsertAdministratorForm {
+    private String name;
+    private String mailAddress;
+    private String password;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getMailAddress() {
+        return mailAddress;
+    }
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    @Override
+    public String toString() {
+        return "InsertAdministratorForm [name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
+                + "]";
+    }
+
     
 }
